5b2d88a5b422ff1b70c787acd57b4d4d22fc2475
Jay Knott <knotjay@gmail.com>
2025-08-29 19:15:00 +0200

    Various docker scripts, dictated by ChatGPT-5. Make sure to run them in the right order.

--- docker-db-shell.sh
+++ docker-db-shell.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
-
-docker exec -i new_docker_built_rails_app-db-1 mysql -u root fakehatecrimesdevelopment < db/database.sql.txt
-docker exec -i new_docker_built_rails_app-db-1 mysql -u root fakehatecrimestest < db/database.sql.txt
-echo "Running Docker shell from which you can run mysql..."
-docker-compose exec db bash
-
+echo "🐚 Opening MySQL shell inside the db container..."
+docker-compose exec db mysql -u root
--- docker-start.sh
+++ docker-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "In one of the bash shells with the prompt root@AHEXNUMBER:/app# enter rails server -b 0.0.0.0 -p 3000"
+echo "In one of the bash shells with the prompt root@AHEXNUMBER:/app# enter rails server -b 0.0.0.0 -p 3003"
 
 echo "🚀 Starting containers..."
 docker-compose up -d
