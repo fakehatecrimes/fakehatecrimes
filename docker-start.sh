5bcc516c284f7b931d1b631fc1d2292a29ecfcf5
fakehatecrimes <knotjay@gmail.com>
2026-03-09 13:52:59 +0700

    Added message on 'Join' page explaining what is a valid password.

--- docker-start.sh
+++ docker-start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-echo "In one of the bash shells with the prompt root@AHEXNUMBER:/app# enter rails server -b 0.0.0.0 -p 3003 or run script docker-rails-3003.sh"
+echo "In one of the bash shells with the prompt root@AHEXNUMBER:/app# enter "
+echo "rails server -b 0.0.0.0 -p 3003 "
+echo "or run script docker-rails-3003.sh"
 
 echo "🚀 Starting containers..."
 docker-compose up -d
